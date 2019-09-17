db2e550dd8193d2763350333717db0ad4b81e52b
MagicMan <microblogworkingmail@mail.ru>
2019-09-17 03:17:06 +0300

    new

--- src/components/common/Pagination/Pagination.jsx
+++ src/components/common/Pagination/Pagination.jsx
@@ -13,9 +13,17 @@ const Pagination = (props) => {
     const[rightBorder,setRightBorder] = useState(props.displayingElelements)
     const[pagesPart,setPagesPart] = useState(1) 
     return <div className={style.pagination}>
+    {pagesPart>1?<button onClick={()=>{setLeftBorder(leftBorder-props.displayingElelements) 
+                          setRightBorder(rightBorder-props.displayingElelements)
+                          setPagesPart(pagesPart-1)
+                          props.onPageChanged(leftBorder+1)
+                          }} className={style.button}>Назад</button>:null}
     {pages.slice(leftBorder-1,rightBorder).map(page=><span onClick={(e)=>{props.onPageChanged(page)}} className={props.currentPage===page&&style.activePage}>{`${page} `}</span>)}
-    <button onClick={()=>{setLeftBorder(rightBorder+1) 
-                          setRightBorder(rightBorder+props.displayingElelements)}} className={style.button}>Далее</button>
+    {pagesPart<numberOfParts?<button onClick={()=>{setLeftBorder(rightBorder+1) 
+                          setRightBorder(rightBorder+props.displayingElelements)
+                          setPagesPart(pagesPart+1)
+                          props.onPageChanged(rightBorder+1)
+                          }} className={style.button}>Далее</button>:null}
     </div>
     }
     
