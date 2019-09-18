6af3bf32f9008c6b8e43a9a533dbbc1bd8462d53
MagicMan <microblogworkingmail@mail.ru>
2019-09-18 08:33:21 +0300

    new

--- src/components/Navigation/BestFriends.jsx
+++ src/components/Navigation/BestFriends.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {useState} from 'react'
 import style from './BestFriends.module.css';
 import Preloader from './../common/Preloader/Preloader'
 import {NavLink} from 'react-router-dom';
 import {withRouter} from "react-router";
 import {connect} from "react-redux"
 import {setCurrentProfile} from '../../redux/users-reducer'
+import {getUserStatusThunk, setUserProfileThunk} from "../../redux/profile-reducer"
 
 const BestFriends = (props) => {
     return (
@@ -14,20 +14,22 @@ const BestFriends = (props) => {
     <section className={style.bestfriends_persons}>
           <p>Друзья</p>
           <ul>
-          <li><NavLink onClick={()=>{setCurrentProfile(props.bestFriends[0].id)}} to={`/profile/${props.bestFriends[0].id}`}>{props.bestFriends[0].name}</NavLink></li>
-          <li><NavLink onClick={()=>{setCurrentProfile(props.bestFriends[1].id)}} to={`/profile/${props.bestFriends[1].id}`}>{props.bestFriends[1].name}</NavLink></li>
-          <li><NavLink onClick={()=>{setCurrentProfile(props.bestFriends[2].id)}} to={`/profile/${props.bestFriends[2].id}`}>{props.bestFriends[2].name}</NavLink></li>
+          <li><NavLink onClick={()=>{
+                props.setUserProfileThunk(props.bestFriends[0].id)
+                props.getUserStatusThunk(props.bestFriends[0].id)
+                }} to={`/profile/${props.bestFriends[0].id}`}>{props.bestFriends[0].name}</NavLink></li>
+          <li><NavLink onClick={()=>{props.setUserProfileThunk(props.bestFriends[1].id)
+                                     props.getUserStatusThunk(props.bestFriends[1].id)
+      }} to={`/profile/${props.bestFriends[1].id}`}>{props.bestFriends[1].name}</NavLink></li>
+          <li><NavLink onClick={()=>{props.setUserProfileThunk(props.bestFriends[2].id)
+      props.getUserStatusThunk(props.bestFriends[2].id)
+      }} to={`/profile/${props.bestFriends[2].id}`}>{props.bestFriends[2].name}</NavLink></li>
           </ul>
     </section>:<Preloader />} 
     </div>)}
 
-let mapStateToProps = (state) => {
-      return {
 
-      }
-}
-
-const BestFriendsContain = connect(mapStateToProps)(BestFriends)
+const BestFriendsContain = connect(null,{getUserStatusThunk,setUserProfileThunk})(BestFriends)
 
 const BestFriendsWithRouter = withRouter(BestFriendsContain)
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,17 +3,18 @@ import PostsContain from './Posts/PostsContain'
 import ProfileInfoWithRouter from './ProfileInfo/ProfileInfo'
 import style from './Profile.module.css';
 
-const Profile = React.memo(props => {
+const Profile = (props) => {
     /*
     if we dont use PureComponent
     shouldComponentUpdate(nextProps, nextState) {
         return nextProps!=this.props||nestState!=this.state
     }*/
+    console.log(props)
     return (
     <div>
      <div className={style.profile__hero}></div>
         <ProfileInfoWithRouter currentProfile={props.currentProfile} status = {props.status} profile={props.profile} updateStatusThunk={props.updateStatusThunk} autorizedUserId={props.autorizedUserId}/>
-        {props.userId==props.autorizedUserId?<PostsContain userName={props.userName}/>:null}
-    </div>)})
+        {props.userId==props.autorizedUserId||props.userId==undefined?<PostsContain userName={props.userName}/>:null}
+    </div>)}
 
 export default Profile;
