85e01b1f39a6b4b2a8ccf3c7057f83550f144583
MagicMan <microblogworkingmail@mail.ru>
2019-11-17 15:54:40 +0300

    responsive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import {NavLink} from 'react-router-dom'
 const Header = (props) => {
 
 return (<header className={styles.header}>
-        <p className={styles.header__name}>UNKNOW UNTITLED SOCIAL NETWORK</p>
+        <p className={styles.header__name}>UNKNOWN UNTITLED SOCIAL NETWORK</p>
         {props.userData.isAuth?
         <div className={styles.header__logined}>
         <span className={styles.header__welcome}>Welcome, {props.userData.login}</span><button onClick={props.logout} className={styles.button_logout}>Выйти</button>
