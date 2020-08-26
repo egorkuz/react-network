5eaf2a4a75390168d265a4f82396064638af6b92
egorkuz <51762580+egorkuz@users.noreply.github.com>
2020-08-26 13:38:58 +0300

    Update news-reducer.js

--- src/redux/news-reducer.js
+++ src/redux/news-reducer.js
@@ -69,23 +69,3 @@ export const deleteLike = (newsId,userId) => async (dispatch) => {
 }
 
 export default newsReducer
-
-/*{newsId: 1, 
-    newsName: "Новый особняк стоимостью 25 млн$ был построен на 25 линии СНТ Дружба",
-    newsImage: "https://www.grekomania.ru/images/places/19/cyclades/santorini/big/88316_Famous-Santorini-sunset-3.jpg",
-    date: "28 июля",
-    newsText: "Роскошный особняк смотрится просто изумительно! Рядом с домом стоит множество суперкаров! В доме 8 спален, 5 ванных комнат. Роскошный особняк класса люкс принадлежит русскому бизнессмену!",
-    commentaries: []
-},
-    {newsId: 2, newsName: "Прощальный костер состоится 25 августа",
-    newsImage: "https://www.grekomania.ru/images/places/19/cyclades/santorini/big/88316_Famous-Santorini-sunset-3.jpg",
-    date: "28 июля",
-    newsText: "Будет очень весело! Приедут лучшие диджеи мира. Дом переньон и Хенеси икс о за счёт садоводства",
-    commentaries: []
-},
-    {newsId: 3, newsName: "Дорогое шампанское будет литься рекой на прощальном костре!",
-    newsImage: "https://www.grekomania.ru/images/places/19/cyclades/santorini/big/88316_Famous-Santorini-sunset-3.jpg",
-    date: "28 июля",
-    newsText: "Будет очень весело! Приедут лучшие диджеи мира. Дом переньон и Хенеси икс о за счёт садоводства",
-    commentaries: []  
-},*/
