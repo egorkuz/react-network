72aaf44748c285cd27e24e29b68f49e8cbee8795
egorkuz <51762580+egorkuz@users.noreply.github.com>
2020-08-26 13:18:52 +0300

    Update redux-store.js

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,7 +1,6 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
-import navigationReducer from './navigation-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer'
 import appReducer from './app-reducer'
@@ -13,7 +12,6 @@ import {reducer as formReducer} from 'redux-form'
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
-    navigation: navigationReducer,
     usersPage: usersReducer,
     newsPage: newsReducer,
     auth: authReducer,
